a4e4044575f364cb2e68a2a035bb1d9204b86b52
Logan Parrish <logan@techtonicgroup.com>
2015-02-06 12:27:25 -0700

    Fixed logic
    Player cannot use an attack they do not have, mavis gets  a free shot

--- GrannyFighter2K14V2/GrannyFighter2K14V2/Player.cs
+++ GrannyFighter2K14V2/GrannyFighter2K14V2/Player.cs
@@ -8,7 +8,7 @@ namespace GrannyFighter2K14V2
 {
     class Player
     {
-        public enum AttackType { Slap = 1, Loofah, Heal }
+        public enum AttackType { Slap = 1, Loofah, Heal}
 
         public string Name { get; set; }
         public int HP { get; set; }
@@ -39,105 +39,117 @@ namespace GrannyFighter2K14V2
             Random rng = new Random();
             string attack = Console.ReadLine();
 
-            
-            int trueAttack = int.Parse(attack);
 
-            var Type = (AttackType)trueAttack;
-
-            switch (Type)
+            if (attack != "1" || attack != "2" || attack != "3")
             {
-                case AttackType.Slap:
+                Console.Clear();
+                Console.WriteLine("Use an actual attack, you pleb. Is Mavis' senility rubbing off on you? Mavis gets a free shot.");
+                return 0;
+            }
+            else
+            {
 
-                    if (rng.Next(0, 21) == 20)
-                    {
-                        Console.Clear();
-                        damage = rng.Next(20, 33) * 2;
-                        Console.WriteLine("You landed a *CRIT* and dealt " + damage + " damage!!!");
-                        SlapCount++;
-                        Enemy.HP -= damage;
+                int trueAttack = int.Parse(attack);
 
-                    }
-                    else if (rng.Next(0, 21) == 10 || rng.Next(0, 21) == 15)
-                    {
-                        Console.Clear();
-                        damage = Convert.ToInt32(rng.Next(20, 33) * 1.2);
-                        Console.WriteLine("You landed a *MINI CRIT* and dealt " + damage + " damage!!!");
-                        SlapCount++;
-                        Enemy.HP -= damage;
-                    }
-                    else
-                    {
-                        if (rng.Next(0, 101) <= Accuracy)
+                var Type = (AttackType)trueAttack;
+
+
+
+                switch (Type)
+                {
+
+                    case AttackType.Slap:
+
+                        if (rng.Next(0, 21) == 20)
                         {
-                            damage = rng.Next(18, 33);
                             Console.Clear();
-                            Console.WriteLine("You hit Mavis square on the butt and deal " + damage + " damage.");
+                            damage = rng.Next(20, 33) * 2;
+                            Console.WriteLine("You landed a *CRIT* and dealt " + damage + " damage!!!");
+                            SlapCount++;
+                            Enemy.HP -= damage;
+
+                        }
+                        else if (rng.Next(0, 21) == 10 || rng.Next(0, 21) == 15)
+                        {
+                            Console.Clear();
+                            damage = Convert.ToInt32(rng.Next(20, 33) * 1.2);
+                            Console.WriteLine("You landed a *MINI CRIT* and dealt " + damage + " damage!!!");
                             SlapCount++;
                             Enemy.HP -= damage;
                         }
                         else
                         {
-                            Console.Clear();
-                            Console.WriteLine("You prove your father right, and fail. You missed.");
+                            if (rng.Next(0, 101) <= Accuracy)
+                            {
+                                damage = rng.Next(18, 33);
+                                Console.Clear();
+                                Console.WriteLine("You hit Mavis square on the butt and deal " + damage + " damage.");
+                                SlapCount++;
+                                Enemy.HP -= damage;
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("You prove your father right, and fail. You missed.");
+                            }
                         }
-                    }
 
-                    break;
-                case AttackType.Loofah:
+                        break;
+                    case AttackType.Loofah:
 
-                    if (Loofahs > 0)
-                    {
-                        damage = rng.Next(1, 41);
+                        if (Loofahs > 0)
+                        {
+                            damage = rng.Next(1, 41);
 
-                        Console.Clear();
+                            Console.Clear();
 
-                        Loofahs--;
-                        Console.WriteLine("You pelt Mavis with a loofah and deal " + damage + " damage.");
-                        Enemy.HP -= damage;
-                    }
-                    else if (Loofahs == 0)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("You tried throwing a loofah you didn't have and Mavis gets a free shot!");
-                    }
+                            Loofahs--;
+                            Console.WriteLine("You pelt Mavis with a loofah and deal " + damage + " damage.");
+                            Enemy.HP -= damage;
+                        }
+                        else if (Loofahs == 0)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You tried throwing a loofah you didn't have and Mavis gets a free shot!");
+                        }
 
-                    break;
-                case AttackType.Heal:
+                        break;
+                    case AttackType.Heal:
 
-                    if (rng.Next(0, 21) == 15)
-                    {
-                        Console.Clear();
-                        Console.WriteLine(@"The pill you take was actually freebase cocaine smuggled in by the Mexican Drug Cartel!
+                        if (rng.Next(0, 21) == 15)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(@"The pill you take was actually freebase cocaine smuggled in by the Mexican Drug Cartel!
 You gain an immediate 100 health boost and gain immunity to an extra pill.");
-                        Healing--;
-                        this.HP += 100;
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Healing++;
-                        damage = rng.Next(10, 41);
-                        this.HP += damage;
-                        Console.WriteLine("You pop some pills and heal yourself for " + damage + " health... careful not to OD.");
-                    }
-                    if (Healing == 5)
-                    {
-                        this.HP = 0;
-                    }
-                
-                //if user inputs a number that the system does not recognize
-                if (trueAttack != 1 && trueAttack != 2 && trueAttack != 3)
-            {
-                Console.Clear();
-                Console.WriteLine("You try using something you don't even have, dumb ass. Mavis gets a free shot.");
-            }
+                            Healing--;
+                            this.HP += 100;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Healing++;
+                            damage = rng.Next(10, 41);
+                            this.HP += damage;
+                            Console.WriteLine("You pop some pills and heal yourself for " + damage + " health... careful not to OD.");
+                        }
+                        if (Healing == 5)
+                        {
+                            this.HP = 0;
+                        }
 
-                    break;
-                default:
-                    break;
-            }
-            return damage;
+                        //if user inputs a number that the system does not recognize
+                        if (trueAttack != 1 && trueAttack != 2 && trueAttack != 3)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You try using something you don't even have, dumb ass. Mavis gets a free shot.");
+                        }
 
+                        break;
+                    default:
+                        break;
+                }
+                return damage;
+            }
         }
     }
 }
